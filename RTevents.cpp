ee701f095e665fae6882a8bc27688170a7a14701
hodaig <hodaig@gmail.com>
2016-05-12 19:22:15 +0300

    first phase of using uSec instead of milliseconds

--- RTevents.cpp
+++ RTevents.cpp
@@ -6,17 +6,20 @@
  */
 
 #include "RTevents.h"
+
+#define RT_TIMER_AUTOSTOP
 #include "RTtimer.h"
 
 /* constants */
 #define RT_MAX_MILLIS       0xFFFFFFFF  // ~50 days
+#define RT_MAX_MICROS       0xFFFFFFFF  // ~70 minutes
 
 /* flags */
 #define RT_FLAG_ACTIVE     1      // without this flag the task is not scheduled
 #define RT_FLAG_USED       2
 #define RT_FLAG_ONE_TIME   4      // if set, this task will occur once and removed
 #define RT_FLAG_OVERFLOW   8      // the next occurrence is after the counter is overflow ( > 2^32)
-#define RT_FLAG_UNIT_US    16     // if set, the period / delay is in uSec - TODO - in development, don't use!
+
 #define RT_FLAG_NESTED     32     // this task can be called nested - TODO - in development, don't use!
 
 /* macros */
@@ -25,6 +28,12 @@
 #define RT_CHECK_FLAG(var, flag) (var & flag)
 
 #define RT_CALC_NEXT_TIME(from, delay) ((unsigned long)((unsigned long)from + (unsigned long)delay))   // TODO - add check for overflow
+
+#ifdef RT_UNITS_US
+#  define RT_GET_TIME() micros()
+#else
+#  define RT_GET_TIME() millis()
+#endif
 
 #define RT_LOCK_TABLE()        cli();
 #define RT_RELEASE_TABLE()     sei();
@@ -36,28 +45,31 @@
 /* static variables */
 RTtask_t RTevents::_tasksQueue[RT_QUEUE_SIZE];
 
-unsigned long RTevents::_curentNextIntrerupt;
+volatile unsigned long RTevents::_curentNextIntrerupt;
 
 #ifdef RT_LITE_MEM_MODE
 unsigned long RTevents::_lastUpdate;
 #endif
 
-bool RTevents::_interuptIsActivale;
+volatile bool RTevents::_interuptIsActivate;
     
 
 void RTevents::begin(){
+#ifdef RT_UNITS_US
+    _curentNextIntrerupt = RT_MAX_MICROS;
+#else
     _curentNextIntrerupt = RT_MAX_MILLIS;
-    _interuptIsActivale = false;
+#endif
+    _interuptIsActivate = false;
 
 #ifdef RT_LITE_MEM_MODE
-    _lastUpdate = millis();
+    _lastUpdate = RT_GET_TIME();
 #endif
 
     memset(&_tasksQueue, 0x0, sizeof(_tasksQueue));
 
     // define the timer and attach the interrupt handler
     RTtimer_begin();
-    RTtimer_stop();
     RTtimer_attachInterrupt(RTevents::RTinteruptHandler);
 
 }
@@ -65,11 +77,10 @@ void RTevents::begin(){
 #ifdef RT_LITE_MEM_MODE
 uint8_t RTevents::addTask(RTfunc_t func, uint16_t delay, uint16_t period){
 #else
-uint8_t RTevents::addTask(RTfunc_t func, unsigned long delay, uint16_t period){
+uint8_t RTevents::addTask(RTfunc_t func, uint32_t delay, uint32_t period){
 #endif
     // basic validation
-    if (delay < 0 || delay > RT_MAX_DELAY ||
-                period < 0 || period > RT_MAX_PERIOD){
+    if (delay > RT_MAX_DELAY || period > RT_MAX_PERIOD){
         return 0;
     }
 
@@ -92,16 +103,16 @@ uint8_t RTevents::addTask(RTfunc_t func, unsigned long delay, uint16_t period){
             }
 
 #ifdef RT_LITE_MEM_MODE
-            _tasksQueue[i].leftForNextOccur = delay + (millis()-_lastUpdate);
+            _tasksQueue[i].leftForNextOccur = delay + (RT_GET_TIME()-_lastUpdate);
 #else
-            _tasksQueue[i].nextOccurrence = RT_CALC_NEXT_TIME(millis(), delay);
+            _tasksQueue[i].nextOccurrence = RT_CALC_NEXT_TIME(RT_GET_TIME(), delay);
 #endif
 
             RT_FLAG_ADD(_tasksQueue[i].flags, RT_FLAG_ACTIVE);
             // now this task can be scheduled
 
 #ifdef RT_LITE_MEM_MODE
-            if (0 == _curentNextIntrerupt || RT_CALC_NEXT_TIME(millis(), delay) < _curentNextIntrerupt){
+            if (0 == _curentNextIntrerupt || RT_CALC_NEXT_TIME(RT_GET_TIME(), delay) < _curentNextIntrerupt){
 #else
             if (0 == _curentNextIntrerupt || _tasksQueue[i].nextOccurrence < _curentNextIntrerupt){
 #endif
@@ -130,99 +141,132 @@ void RTevents::RTattachInterrupt(uint16_t delay){
 void RTevents::RTattachInterrupt(unsigned long delay){
 #endif
 
-    _interuptIsActivale = true;
-    _curentNextIntrerupt = millis() + delay;
+    _interuptIsActivate = true;
+    _curentNextIntrerupt = RT_GET_TIME() + delay;
 
+#ifdef RT_UNITS_US
+    if (delay < RT_MIN_TIMER) delay = RT_MIN_TIMER;
+    RTtimer_schedNext_us(delay);
+#else
     RTtimer_schedNext_us(delay*1000);
+#endif
 
 }
 
 void RTevents::RTdetachInterrupt(){
 
-    _interuptIsActivale = false;
+    _interuptIsActivate = false;
+#ifdef RT_UNITS_US
+    _curentNextIntrerupt = RT_MAX_MICROS;
+#else
     _curentNextIntrerupt = RT_MAX_MILLIS;
+#endif
 
     RTtimer_stop();
 
 }
 
 void RTevents::RTinteruptHandler(){
+#ifdef RT_UNITS_US
+    unsigned long nextInterrupt = RT_MAX_MICROS;
+#else
     unsigned long nextInterrupt = RT_MAX_MILLIS;
-    unsigned long localMillis;
+#endif
+
+    unsigned long localMicros;
     // TODO - make 'i' static to solve starvation problems
     // TODO - wrap all this with 'while' loop instead using recursion to solve stack overflow
 
+    static volatile bool lock = false;
+    if (lock){
+        return;
+    } else {
+        lock = true;
+    }
     // first stop the next interrupts
-    RTdetachInterrupt();
+    //interrupts();           // release all the rest interrupts
+    //RTdetachInterrupt();    // already done by RTtimer
 
+    //while(true) {
+        localMicros = RT_GET_TIME();
 #ifdef RT_LITE_MEM_MODE
-    localMillis = millis();
-    unsigned long deltaTime= localMillis - _lastUpdate;
-    // TODO - if negative millis overflow occur
+        unsigned long deltaTime= localMicros - _lastUpdate;
+        // TODO - if negative millis overflow occur
 #endif
 
-    for (int i=0 ; i<RT_QUEUE_SIZE ; i++){
+        for (int i=0 ; i<RT_QUEUE_SIZE ; i++){
+            if (RT_CHECK_FLAG(_tasksQueue[i].flags, RT_FLAG_ACTIVE)) {               /* is an active task */
 #ifdef RT_LITE_MEM_MODE
-        if (_tasksQueue[i].leftForNextOccur <= deltaTime &&                     /* need to be executed now */
+                if(_tasksQueue[i].leftForNextOccur <= deltaTime) {                   /* need to be executed now */
 #else
-        if (_tasksQueue[i].nextOccurrence <= millis() &&                     /* need to be executed now */
+                if(_tasksQueue[i].nextOccurrence <= localMicros) {                   /* need to be executed now */
 #endif
-                    RT_CHECK_FLAG(_tasksQueue[i].flags, RT_FLAG_ACTIVE) &&        /* is an active task */
-                    (!RT_CHECK_FLAG(_tasksQueue[i].flags, RT_FLAG_OVERFLOW)) ){  /* not need to wait for overflow */
-            RTexecuteTask(&_tasksQueue[i]);
-        }
-    }
+                    RTexecuteTask(&_tasksQueue[i]);
+                }
 
-    // schedule the next interrupt
-#ifdef RT_LITE_MEM_MODE
-    RT_LOCK_TABLE();      // lock the tasks table
-#else
-    localMillis = millis();
-#endif
-
-    for (int i=0 ; i<RT_QUEUE_SIZE ; i++){
-        if (RT_CHECK_FLAG(_tasksQueue[i].flags, RT_FLAG_ACTIVE) ||
-                    RT_CHECK_FLAG(_tasksQueue[i].flags, RT_FLAG_USED) ){  /* not need to wait for overflow */
-
-#ifdef RT_LITE_MEM_MODE
-            if (_tasksQueue[i].leftForNextOccur < deltaTime){
-                _tasksQueue[i].leftForNextOccur = 0;
-            } else {
-                _tasksQueue[i].leftForNextOccur -= deltaTime;
+                if (RT_CHECK_FLAG(_tasksQueue[i].flags, RT_FLAG_ACTIVE)) {          /* is still activate */
+                    nextInterrupt = MIN(nextInterrupt, _tasksQueue[i].nextOccurrence);
+                }
             }
-            nextInterrupt = MIN(nextInterrupt, _tasksQueue[i].leftForNextOccur);
+        }
+
+#if 0
+            // schedule the next interrupt
+#ifdef RT_LITE_MEM_MODE
+            RT_LOCK_TABLE();      // lock the tasks table
 #else
-            nextInterrupt = MIN(nextInterrupt, _tasksQueue[i].nextOccurrence);
+        localMicros = RT_GET_TIME();
+        nextInterrupt = RT_MAX_MICROS;
 #endif
 
-        }
-    }
+        for (int i=0 ; i<RT_QUEUE_SIZE ; i++){
+            if (RT_CHECK_FLAG(_tasksQueue[i].flags, RT_FLAG_ACTIVE)){  /* is activate */
 
 #ifdef RT_LITE_MEM_MODE
-    _lastUpdate+=deltaTime;                 // the time we start that function
-    RT_RELEASE_TABLE();                     // release the table
-    deltaTime = millis() - _lastUpdate;     // how much time we lost during that function
+                if (_tasksQueue[i].leftForNextOccur < deltaTime){
+                    _tasksQueue[i].leftForNextOccur = 0;
+                } else {
+                    _tasksQueue[i].leftForNextOccur -= deltaTime;
+                }
+                nextInterrupt = MIN(nextInterrupt, _tasksQueue[i].leftForNextOccur);
+#else
+                nextInterrupt = MIN(nextInterrupt, _tasksQueue[i].nextOccurrence);
+#endif
+
+            }
+        }
+
+#ifdef RT_LITE_MEM_MODE
+        _lastUpdate+=deltaTime;                 // the time we start that function
+        RT_RELEASE_TABLE();                     // release the table
+        deltaTime = RT_GET_TIME() - _lastUpdate;     // how much time we lost during that function
+#endif
+
 #endif
 
 #ifdef RT_LITE_MEM_MODE
-    if (nextInterrupt < deltaTime){
-        // still have tasks to execute
-        RTattachInterrupt(0);
-    } else {
-        if (nextInterrupt != RT_MAX_MILLIS){
-            RTattachInterrupt(nextInterrupt - deltaTime);
+        if (nextInterrupt < deltaTime){
+            // still have tasks to execute
+            RTattachInterrupt(0);
+        } else {
+            if (nextInterrupt != RT_MAX_MILLIS){
+                RTattachInterrupt(nextInterrupt - deltaTime);
+            }
         }
-    }
 #else
-    if (nextInterrupt < localMillis){
-        RTattachInterrupt(0);
-    } else {
-        if (nextInterrupt != RT_MAX_MILLIS){
-            RTattachInterrupt(nextInterrupt - localMillis);
+        if (nextInterrupt != RT_MAX_MICROS){
+            //nextInterrupt += localMicros;
+            //localMicros = RT_GET_TIME();
+            //nextInterrupt -=localMicros;
+            if (nextInterrupt < localMicros){
+                RTattachInterrupt(0);
+            } else {
+                RTattachInterrupt(nextInterrupt - localMicros);
+            }
         }
-    }
-#endif
 
+#endif
+    lock = false;
 }
 
 void RTevents::RTexecuteTask(RTtask_t* theTask){
@@ -231,18 +275,21 @@ void RTevents::RTexecuteTask(RTtask_t* theTask){
     // halt next execution of this task
     RT_FLAG_DEL(theTask->flags, RT_FLAG_ACTIVE);
 
+#if 0
     if (RT_CHECK_FLAG(theTask->flags, RT_FLAG_NESTED)) {
-    	sei(); // TODO - allow nested interupt
+        sei(); // TODO - allow nested interupt
     }
-    
+#endif
+
     // execute the task 
     func();
-
+#if 1
     // check if this task was deleted
     if (func != theTask->func ||
                 RT_CHECK_FLAG(theTask->flags, RT_FLAG_ACTIVE)){
         return;
     }
+#endif
 
     if (RT_CHECK_FLAG(theTask->flags, RT_FLAG_ONE_TIME)){
         // free this task (not needed any more
--- RTevents.h
+++ RTevents.h
@@ -18,17 +18,21 @@
 // un-commernt this to use less memory - TODO - experimental mode
 // #define RT_LITE_MEM_MODE
 
+#define RT_UNITS_US
+
 /* constants */
 #ifdef RT_LITE_MEM_MODE
 # define RT_MAX_DELAY        0xFFFF
 #else
 # define RT_MAX_DELAY        0xEFFFFFFF
 #endif
-#define RT_MAX_PERIOD       0xFFFF
+#define RT_MAX_PERIOD        0xFFFFFFFF
+
+#define RT_MIN_TIMER         12
 
 // change this to the expected maximum number of task at the same time
 // each task take sizeof(RTtask) bytes of RAM (9byte)
-#define RT_QUEUE_SIZE          20		
+#define RT_QUEUE_SIZE          10
 
 
 /* public types */
@@ -37,15 +41,15 @@ typedef void (*RTfunc_t) (void);
 
 /* internal tasks struct */
 typedef struct {
-    RTfunc_t func;                      // void (*func) (void);
-    uint8_t flags;                    // see:  RT_FLAG_...
+    volatile RTfunc_t func;                    // void (*func) (void);
+    volatile uint8_t flags;                    // see:  RT_FLAG_...
 
 #ifdef RT_LITE_MEM_MODE
     uint16_t leftForNextOccur;        // the time left till the next execution
 #else
-    unsigned long nextOccurrence;     // the next time this task will execute
+    volatile unsigned long nextOccurrence;     // the next time this task will execute
 #endif
-    uint16_t period;                  // time in milliseconds before every occurrence
+    volatile uint32_t period;                  // time in milliseconds before every occurrence
 } RTtask_t;
 
 
@@ -54,13 +58,13 @@ private:
 
     static RTtask_t _tasksQueue[RT_QUEUE_SIZE];
 
-    static unsigned long _curentNextIntrerupt;
+    static volatile unsigned long _curentNextIntrerupt;
 
 #ifdef RT_LITE_MEM_MODE
     static unsigned long _lastUpdate;
 #endif
 
-    static bool _interuptIsActivale;
+    static volatile bool _interuptIsActivate;
 
 public:
 
@@ -69,7 +73,7 @@ public:
 #ifdef RT_LITE_MEM_MODE
     static uint8_t addTask(RTfunc_t func, uint16_t delay, uint16_t period);
 #else
-    static uint8_t addTask(RTfunc_t func, unsigned long delay, uint16_t period);
+    static uint8_t addTask(RTfunc_t func, uint32_t delay, uint32_t period);
 #endif
 
     static bool removeTask(uint8_t taskID);
